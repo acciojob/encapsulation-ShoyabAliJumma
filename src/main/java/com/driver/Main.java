f4f8bd25b9ceb47125d6450bc96962182d2a8c22
ShoyabAliJumma <shoyabalipathan@gmail.com>
2023-01-13 19:24:47 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,11 @@
 package com.driver;
 
 public class Main {
-     class main{
-         public RWOnly getName() {
-             return Name;
-         }
 
-         public void setName(RWOnly Name) {
-             this.Name = Name;
-         }
-
-         RWOnly Name=new RWOnly();
-
-    }
     public static void main(String[] args) {
-
+RWOnly obj=new RWOnly();
+obj.setName("shoyab");
+        System.out.println(obj.getName());
     }
   
 }
